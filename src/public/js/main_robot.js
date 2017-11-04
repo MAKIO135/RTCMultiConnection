cac66ef47a00ad5bdfd164c4cfc68afcd2e774f6
RADISSON lionel <lionel.radisson@gmail.com>
2017-11-04 12:41:59 +0100

    deactivate BT

--- src/public/js/main_robot.js
+++ src/public/js/main_robot.js
@@ -54,7 +54,7 @@ document.querySelector( '#open-room' ).addEventListener( 'click', () => {
             socket.on( 'cmd', data => {
                 console.log( data );
                 if( data.roomid === roomid ){
-                    terminal.send( data.cmd ).then( () => console.log( data, 'out' ) ).catch( error => console.log( error ) );
+                    // terminal.send( data.cmd ).then( () => console.log( data, 'out' ) ).catch( error => console.log( error ) );
                 }
             } );
         } );
